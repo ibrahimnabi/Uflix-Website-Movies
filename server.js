0c0e90969a7f317d422f86291385bba720ecec56
ibrahimnabi80 <ibrahimnabi80@gmail.com>
2019-12-14 17:59:34 +0500

    helloworld

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(cors());
 
 app.use(require("./routes/routes"));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 80;
 
 app.use("/uploads/", express.static(path.join(__dirname, "uploads")));
 app.use(express.static(path.join(__dirname, "admin")));
